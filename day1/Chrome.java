37b6b614d515570fa1786de8e77dcbbd68427eb4
amraj-cg <a.mohanraj@gmail.com>
2025-05-25 00:02:59 +0530

    Add files via upload
    Day 1 Assignments

---
+++ day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version = 91.0f;
+		String developer = "Google";
+		Boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Version number is: " + version);
+		System.out.println("Developer is: " + developer);
+		System.out.println("ReleaseYear is: " + releaseYear);
+		System.out.println("ShortCutKey is: " + shortcutKey);
+	}
+
+}
